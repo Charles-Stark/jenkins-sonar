41326d8d1af874741ac849a8bd6305257fd53dba
charles_ying <charles.ying@yuansuan.com>
2025-06-24 10:39:36 +0800

    update

--- .cicd/test.sh
+++ .cicd/test.sh
@@ -5,8 +5,8 @@ echo ">>> 创建 venv 并安装依赖"
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install pytest pytest-cov
+pip install pytest pytest-cov allure-pytest
 
 echo ">>> 运行测试"
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-pytest --junit-xml=test-results.xml --cov=app --cov-report=xml --cov-report=html tests/
+pytest --junit-xml=test-results.xml --cov=app --cov-report=xml --cov-report=html --alluredir=allure-results tests/
